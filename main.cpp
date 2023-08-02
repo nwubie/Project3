dbebe580a8b5244043797bc2bae581ad0490b3b3
s3luu <sabrinaluu@ufl.edu>
2023-08-02 13:02:41 -0400

    Valid input for menu

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-\#include <iomanip>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -56,49 +56,65 @@ bool LoadFile(string filename, string& file_content) {
 }
 
 int main() {
-    // Prompts user to input a .txt file to load, file must be in 'cmake-build-debug' folder
-    string filename;
-    string file_content = "";
-    bool validFile = false;
-    do {
-        cout << "Load a .txt file:" << endl;
-        cin >> filename;
+    bool program = true;
+    while (program) {
+        // Prompts user to input a .txt file to load, file must be in 'cmake-build-debug' folder
+        string filename;
+        string file_content = "";
+        bool validFile = false;
+        do {
+            cout << "Load a .txt file:" << endl;
+            cin >> filename;
+            
+            validFile = LoadFile(filename, file_content);
+        } while (!validFile);
         
-        validFile = LoadFile(filename, file_content);
-    } while (!validFile);
+        // Prompts user with 3 options
+        cout << "Please select an option:" << endl;
+        cout << "1. Knuth-Morris-Pratt (KMP) Search" << endl;
+        cout << "2. Boyer-Moore (BM) Search" << endl;
+        cout << "3. Exit" << endl;
     
-    // Prompts user with 3 options
-    cout << "Please select an option:" << endl;
-    cout << "1. Knuth-Morris-Pratt (KMP) Search" << endl;
-    cout << "2. Boyer-Moore (BM) Search" << endl;
-    cout << "3. Exit" << endl;
-
-    int option;
-    cin >> option;
-
-    string word;
-
-    switch(option)
-    {
-        //
-        case 1:
-            cout << "Enter a string to 'KMP' Search:" << endl;
-            cin >> word;
-            // KMPSearch(file_content, word);
-            break;
-        //
-        case 2:
-            cout << "Enter a string to 'BM' Search:" << endl;
-            cin >> word;
-            clock_t start, stop; //time/clock function to measure seconds https://www.geeksforgeeks.org/measure-execution-time-with-high-precision-in-c-c/
-            start = clock(); 
-            BMSearch(file_content, word);
-            stop = clock();
-            double duration = double(stop - start) / double(CLOCKS_PER_SEC);
-            cout << "Time: " << fixed << setprecision(7) << duration << " seconds";
-        // Exits the program
-        
+        int option = -1;
+        do {
+            cin >> option;
+            if (option < 1 || option > 3) {
+                cout << "Enter a valid option!" << endl;
+            }
+        } while (option < 1 || option > 3);
+        string word;
+    
+        switch(option)
+        {
+            //
+            case 1: {
+                cout << "Enter a string to 'KMP' Search:" << endl;
+                cin >> word;
+                // KMPSearch(file_content, word);
+                continue;
+            }
+            //
+            case 2: {
+                cout << "Enter a string to 'BM' Search:" << endl;
+                cin >> word;
+                clock_t start, stop; //time/clock function to measure seconds https://www.geeksforgeeks.org/measure-execution-time-with-high-precision-in-c-c/
+                start = clock(); 
+                BMSearch(file_content, word);
+                stop = clock();
+                double duration = double(stop - start) / double(CLOCKS_PER_SEC);
+                cout << "Time: " << fixed << setprecision(7) << duration << " seconds" << endl << endl;
+                continue;
+            }
+            // Exits the program
+            case 3: {
+                program = false;
+                break;
+            }
+            default: {
+                program = true;
+                continue;
+            }
+        }
     }
-
     return 0;
 }
